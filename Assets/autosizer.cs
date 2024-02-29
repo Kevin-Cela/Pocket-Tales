8a4e3bfec200474935956f59e0a501b1324dc0b0
Kevin Cela <kcela21@epoka.edu.al>
2024-02-29 22:09:04 +0100

    Improved on the autoscaler

--- Assets/autosizer.cs
+++ Assets/autosizer.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class autosizer : MonoBehaviour
 {
+    [SerializeField] private float defaultScreenSize;
     [SerializeField] private Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
         float screenWidth = canvas.GetComponent<RectTransform>().rect.width;
-        this.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(screenWidth, this.GetComponentInChildren<RectTransform>().rect.height);
-        //this.transform.localScale = new Vector3(screenWidth, this.transform.localScale.y, this.transform.localScale.z);
+        for (int childCount = 0; childCount < this.transform.childCount; childCount++)
+        {
+            Transform currentChild = this.transform.GetChild(childCount);
+            if (currentChild.name == "Square")
+            {
+                currentChild.GetComponent<RectTransform>().localScale = new Vector3(screenWidth, currentChild.GetComponent<RectTransform>().localScale.y, currentChild.GetComponent<RectTransform>().localScale.z);
+            }
+            else if(currentChild.GetComponents<TextMeshProUGUI>().Length > 0)
+            {
+                currentChild.GetComponent<RectTransform>().sizeDelta = new Vector2(screenWidth, currentChild.GetComponent<RectTransform>().rect.height);
+            }
+            else if (currentChild.tag == "Option")
+            {
+                float scaleMultiplier = screenWidth / defaultScreenSize;
+                currentChild.GetComponent<RectTransform>().localScale = new Vector3(currentChild.GetComponent<RectTransform>().localScale.y, currentChild.GetComponent<RectTransform>().localScale.y, currentChild.GetComponent<RectTransform>().localScale.z) * scaleMultiplier;
+                currentChild.GetComponent<RectTransform>().position = new Vector3(currentChild.GetComponent<RectTransform>().position.x * scaleMultiplier, currentChild.GetComponent<RectTransform>().position.y, currentChild.GetComponent<RectTransform>().position.z);
+            }
+        }
+        
     }
 
     // Update is called once per frame
